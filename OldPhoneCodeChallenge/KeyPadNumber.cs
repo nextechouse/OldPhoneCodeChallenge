a29b2e2f953ee331465303e66d2432ae091f48bb
nextechouse <nextechouse@gmail.com>
2022-10-18 00:31:28 +0700

    Initial commit

---
+++ OldPhoneCodeChallenge/KeyPadNumber.cs
@@ -0,0 +1,7 @@
+namespace OldPhoneCodeChallenge
+{
+    public class KeyPadNumber
+    {
+        
+    }
+}
---
+++ OldPhoneCodeChallenge/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace OldPhoneCodeChallenge
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+    }
+  }
+}
